1dde28c0ccf6686fa33fd290c54e598338c44426
Stefano Fago <stefanofago@gmail.com>
2013-08-31 17:40:58 +0200

    Version to use with -verbose:class
    New Version of original project with some fix:
    1) easy demostration of class unloading suing -verbose:class flag,
    triggering GC
    2) non blocking commands using SwingUtils
    3) new item on menu to see differents unloading behaviours

--- src/com/application/ui/Main.java
+++ src/com/application/ui/Main.java
@@ -9,25 +9,33 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /**
  * @author Fabio Falci
- * 
+ *
+ * FIX:
+ *       - added triggerGC functionality to use with java -verbose:class
+ *         to demostrate unloading...  		
+ *       - added close-all menu and trigger-gc menu
+ *
  */
+ 
 public class Main extends JPanel {
 
 	private ViewArea viewArea;
-	
+
 	public Main() {
 		buildMenu();
 		viewArea = new ViewArea();
 		setLayout(new BorderLayout());
 		add(viewArea.getViewComponent());
 	}
+
 	private void buildMenu() {
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menu = new JMenu("Views");
-		
+
 		JMenuItem menuItem = new JMenuItem("View A");
 		menu.add(menuItem);
 		menuItem.addActionListener(new ActionListener() {
@@ -36,7 +44,7 @@ public class Main extends JPanel {
 				openViewA();
 			}
 		});
-		
+
 		menuItem = new JMenuItem("View B");
 		menu.add(menuItem);
 		menuItem.addActionListener(new ActionListener() {
@@ -45,20 +53,66 @@ public class Main extends JPanel {
 				openViewB();
 			}
 		});
-		
+
+		menuItem = new JMenuItem("Clean Views");
+		menu.add(menuItem);
+		menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cleanTabs();
+			}
+		});
+
+		menuItem = new JMenuItem("Trigger GC");
+		menu.add(menuItem);
+		menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				SwingUtilities.invokeLater(new Runnable() {
+
+					@Override
+					public void run() {
+						triggerGC();
+					}
+				});
+			}
+		});
+
 		menuBar.add(menu);
 		frame.setJMenuBar(menuBar);
 	}
-	
+
 	protected void openViewB() {
 		viewArea.open("com.application.ui.views.ViewB");
-		
+
 	}
+
+	protected void cleanTabs() {
+		viewArea.close();
+	}
+
 	protected void openViewA() {
 		viewArea.open("com.application.ui.views.ViewA");
 	}
 
+	protected static final void triggerGC() {
+		System.out.println("\n-- Starting GC");
+		int c = 50;
+		while (c-- > 0)
+			System.gc();
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("-- End of GC\n");
+	}
+
 	static JFrame frame;
+
 	public static void main(String[] args) {
 		frame = new JFrame("Unloading class");
 		frame.setContentPane(new Main());
